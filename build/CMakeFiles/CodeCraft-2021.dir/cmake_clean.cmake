9ba654b023685a833625058364cf8b890aaeeb25
eaglesunshine <18900927316@163.com>
2021-03-11 00:16:25 +0800

    build success

---
+++ build/CMakeFiles/CodeCraft-2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeCraft-2021.dir/CodeCraft-2021.cpp.o"
+  "CMakeFiles/CodeCraft-2021.dir/ProcessFile.cpp.o"
+  "CMakeFiles/CodeCraft-2021.dir/Schedule.cpp.o"
+  "/home/lin/workplace/LWP/bin/CodeCraft-2021.pdb"
+  "/home/lin/workplace/LWP/bin/CodeCraft-2021"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeCraft-2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
